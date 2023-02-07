0af966af676432454a95ec1cc47deee8e13b934b
mwest0101 <62764107+mwest0101@users.noreply.github.com>
2023-02-07 12:19:43 -0300

    =çSe agregaron parametro y mejoro optimizacion de directorios

--- functions.py
+++ functions.py
@@ -5,12 +5,17 @@ import shutil
 import argparse
 import locale
 from datetime import datetime
+import re 
+
 locale.setlocale(locale.LC_ALL, '')
 
 def createFolder(cadena_folder):
-   if not os.path.exists(cadena_folder):
-                  os.makedirs(cadena_folder)
-                  
+   try:                  
+      if not os.path.exists(cadena_folder):
+         os.makedirs(cadena_folder)
+   except:
+      print ("Can't Create Folder : "+cadena_folder)
+      
 def removeExtrangeChars(cadena):
    cadena=cadena.replace("\r","")
    cadena=cadena.replace("\n","")
@@ -20,7 +25,7 @@ def removeExtrangeChars(cadena):
    return cadena
 
 def buscar_fecha(cadena):
-   patron = re.compile(r'\d{1,2}[/-]\d{1,2}[/-]\d{4}')
+   patron = re.compile('\d{1,2}[/-]\d{1,2}[/-]\d{4}')
    fecha = re.search(patron, cadena)
    if fecha:
       print(fecha.group())
@@ -44,60 +49,152 @@ def cleanStrangeChars(lineas,cadena):
    return cadena
 
    
-
-def remove_date(cadena):
-   patron = re.compile(r'\d{1,2}[/-]\d{1,2}[/-]\d{4}')
+def searchRegAndReplace(cadena,cadRegExp):   
+   patron = re.compile(cadRegExp)
    fecha = re.search(patron, cadena)
    if fecha:
-      #print("encontre "+fecha.group())
       cadena=cadena.replace(fecha.group(), '')
+   return cadena
+
+
+def remove_date(cadena):
+   cadena=searchRegAndReplace(cadena,"\d{1,2}[/-]\d{1,2}[/-]\d{4} \[.*\]")
+   cadena=searchRegAndReplace(cadena,"\d{1,2}[/-]\d{1,2}[/-]\d{4}")
+   cadena=searchRegAndReplace(cadena,"\d{4}[/-]\d{1,2}[/-]\d{1,2} \[.*\]")
+   cadena=searchRegAndReplace(cadena,"\d{4}[/-]\d{1,2}[/-]\d{1,2}")
+   cadena=searchRegAndReplace(cadena,"\d{4}[/-]\d{1,2}")                              
+   cadena=searchRegAndReplace(cadena,"\d{1,2}[/-]\d{4}")
+
    return cadena   
-   
-   
-def move_files(src_folder,tar_folder,deldate,delstr,addNameFileToNewNameFile,addNameFileToD):
+
+
+def remove_first_char(string):
+    if string[0] == "_":
+        return string[1:]
+    return string
+    
+def getLastFolder(cadena):
+   directory = os.path.dirname(cadena)
+   base = os.path.basename(directory)
+   return base
+
+def move_files(src_folder,tar_folder,
+               move_files,copy_files,create_folder,
+               delStrFromFileName,delDateFromFileName,delStrFromFolderName,delDateFromFolderName,
+               addDateToFileName,addFolderToFileName,addFolderToNewFolder):
    
         for dirpath, dirnames, filenames in os.walk(src_folder):
             for file in filenames:
                file_path = os.path.join(dirpath, file)
+               newFile=file
+               print ("===========================================================================")  
+               
+               lastFolder=getLastFolder(file_path)
+               
+               print("file_path="+file_path)
+               print("lastFolder="+lastFolder)
+                # --------------------------------------------------               
+               # -----DELETE----
+               
+                # Delete string contained in the file "removeStrFromFiles" 
+               # from the file name
+               if (delStrFromFileName==True):
+                  newFile=cleanStrangeChars(readRemoveFile(),newFile) 
+               
+               # Delete the date from the file name                         
+               if (delDateFromFileName==True):
+                  newFile=remove_date(newFile) 
+
+               # Delete string contained in the file "removeStrFromFiles" 
+               # from the folder name                     
+               if (delStrFromFolderName==True):
+                  lastFolder=cleanStrangeChars(readRemoveFile(),lastFolder) 
+                  
+               # Delete the date from the folder name                                              
+               if (delDateFromFolderName==True):
+                  lastFolder=remove_date(lastFolder) 
+                  
+               # --------------------------------------------------                  
+               # ------------------ADD-----------------------------
+               # Add de date to the start of file name
+               addToNewFile=""
+               addToNewFolder=""
+               
+               if (addDateToFileName==True):   
+                  print ("Adding date = [ "+modify_date.strftime("%Y-%m-%d")+" ] to file name")
+                  addToNewFile=addToNewFile+"_"+modify_date.strftime("%Y-%m-%d")
+                  
+
+               # Add de original folder name to the begin of the new file name
+               if (addFolderToFileName==True):  
+                  print ("Adding last folder = [ "+lastFolder+" ] to file name")
+                  addToNewFile=addToNewFile+"_"+lastFolder
+                                             
+               # Add de original folder name to the begin of the new folder name
+               if (addFolderToNewFolder==True):   
+                  print ("Adding last folder = [ "+lastFolder+"  ] to folder name")
+                  addToNewFolder=addToNewFolder+"_"+lastFolder
+                  
+              
+               
                modify_date = datetime.fromtimestamp(os.path.getmtime(file_path))
                 
                dest_folder = os.path.join(tar_folder, modify_date.strftime("%Y"))
-               createFolder(dest_folder)
+               
+               
+               if (move_files or copy_files or create_folder):
+                  createFolder(dest_folder)
+                  
                     
                dest_folder = dest_folder + "\\" + modify_date.strftime("%m - %B")
-               createFolder(dest_folder)
+               
+               if (move_files or copy_files or create_folder):
+                  createFolder(dest_folder)                  
                     
                dest_folder = dest_folder + "\\" + modify_date.strftime("%Y-%m-%d [ %A ]")
                
-               newFile=file
                
-                  
-               if (delStrFromFileName==True):
-                  newFile=filterFolderFile(newFile) 
-                     
-               if (delStrFromFolderName==True):
-                  dest_folder=filterFolderFile(dest_folder) 
-                  
-               if (delDateFromFileName==True):
-                  newFile=remove_date(newFile) 
-                     
-               if (delDateFromFolderName==True):
-                  dest_folder=remove_date(dest_folder) 
-                     
-               createFolder(dest_folder)
+                #concateno y creo ruta destino y nombre de archivo destino completo 
+               newFile=addToNewFile+newFile
+               dest_folder=dest_folder+addToNewFolder
+               dest_folder=dest_folder.strip()
+               dest_folder=remove_first_char(dest_folder)
                
-               print ("==========================================")                            
-               print ("Moving from: "+file_path)        
-               print ("Moving   to:" +dest_folder+"\\" +file)    
+               newFile=remove_first_char(newFile)
+               if (move_files or copy_files or create_folder):
+                  createFolder(dest_folder)                     
+               
+               
+    
+               if (move_files):
+                  print("Moving Files...")
+                  
+               if (copy_files):
+                  print("Copy Files...")                  
+                  
+               if (create_folder):
+                  print("Creating Folder...")
+                                       
+               print ("from: "+file_path)        
+               print ("to:" +dest_folder+"\\" +file)    
                # Mover el archivo a la carpeta de destino
-               try:
-           
+               if (move_files or copy_files):
+                  try:            
+                     if  (move_files):
+                        shutil.move(file_path, os.path.join(dest_folder, newFile))
+                     if (copy_files):
+                        shutil.copy(file_path, os.path.join(dest_folder, newFile))
+                  except:
+                     f1 = open(src_folder+'\mddf2_move_log.txt','a')
+                     f1.write('\n' + "I can't move or copy from: "+file_path+"\\" +file)
+                     f1.write('\n' + "I can't move or copy to  : "+dest_folder+"\\" +newFile)
+                     f1.close()  
                      
-                  shutil.move(file_path, os.path.join(dest_folder, newFile))
-               except:
-                  f = open(src_folder+'\mddf2_move_log.txt','a')
-                  f.write('\n' + "I can't move: "+file_path+"\\" +file)
-                  f.close()                    
-                  print ("I can't move: "+file_path+"\\" +file)
+                     f2 = open(tar_folder+'\mddf2_move_log.txt','a')
+                     f2.write('\n' + "I can't move or copy from: "+file_path+"\\" +file)
+                     f2.write('\n' + "I can't move or copy to  : "+os.path.join(dest_folder, newFile))
+                     
+                     f2.close()                    
+                     print ("I can't move or copy: "+file_path+"\\" +file)
                   
                   
--- mddf2.py
+++ mddf2.py
@@ -10,15 +10,25 @@ from functions import *
 
 #locale.setlocale(locale.LC_TIME, 'es_ES')
 locale.setlocale(locale.LC_ALL, '')
+#def move_files(src_folder,tar_folder,
+# delStrFromFileName,delDateFromFileName,delStrFromFolderName,delDateFromFolderName,
+# addDateToFileName,addFolderToFileName,addFolderToNewFolder):
 
 parser = argparse.ArgumentParser()
 parser.add_argument("sd", help="Source directory")
 parser.add_argument("td", help="Target directory")
-parser.add_argument('-ds', '--delstr', action='store_true', help='delete str in directories and files')
-parser.add_argument('-dd', '--deldate', action='store_true', help='delete date str in directories and files')
-parser.add_argument('-anf', '--addNameFolderToFolder', action='store_true', help='add name folder to new folder name')
-parser.add_argument('-and', '--addNameFolderToFile', action='store_true',   help='add name folder to new file name')
+parser.add_argument('-m',   '--move_file', action='store_true', help='move files')
+parser.add_argument('-c',   '--copy_file', action='store_true', help='copy files')
+parser.add_argument('-f',   '--create_folder', action='store_true', help='create folders')
+parser.add_argument('-dsf', '--delStringFilename', action='store_true', help='Delete String in file remove(default: "remove.list") from target filename')
+parser.add_argument('-ddf', '--delDateFilename', action='store_true',   help='Delete date string from target filename')
+parser.add_argument('-dsd', '--delStringDirectory', action='store_true', help='Delete string in file remove(default: "remove.list") from target directory')
+parser.add_argument('-ddd', '--delDateDirectory', action='store_true',  help='Delete date string  from target directory(if source directory was added)')
 
+
+parser.add_argument('-adf', '--addDateToFileName', action='store_true', help='Add date to filename')
+parser.add_argument('-aff', '--addFolderToFileName', action='store_true',   help='Add folder to filename')
+parser.add_argument('-afd', '--addFolderToNewFolder', action='store_true',   help='Add source folder to new folder name')
 
 #parser.add_argument("dn", help="add the date to begint of the file name")
 #parser.add_argument("r", help="add the date to begint of the file name")
@@ -33,20 +43,8 @@ if args.sd:
     if(tar_folder==""):
         tar_folder=src_folder
   
-    move_files(src_folder,tar_folder,args.deldate,args.delstr,args.addNameFolderToFolder,args.addNameFolderToFile)
+    move_files(src_folder,tar_folder,
+               args.move_file,args.copy_file,args.create_folder,
+               args.delStringFilename,args.delStringFilename,args.delStringDirectory,args.delDateDirectory,
+               args.addDateToFileName,args.addFolderToFileName,args.addFolderToNewFolder)
     
-
-
-
-# for dirpath, dirnames, filenames in os.walk(root_dir):
-#     for filename in filenames:
-#         file_path = os.path.join(dirpath, filename)
-#         file_ctime = os.path.getctime(file_path)
-#         file_ctime_date = datetime.datetime.fromtimestamp(file_ctime).date()
-#         year = file_ctime_date.year
-#         month = file_ctime_date.month
-#         new_folder_name = f"{year}-{month}"
-#         new_folder_path = os.path.join(dirpath, new_folder_name)
-#         os.makedirs(new_folder_path, exist_ok=True)
-#         new_file_path = os.path.join(new_folder_path, filename)
-#         os.rename(file_path, new_file_path)
