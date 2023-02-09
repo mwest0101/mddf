e29da3f0f35b466a589c420a6976e4c825df316d
mwest0101 <62764107+mwest0101@users.noreply.github.com>
2023-02-09 02:04:35 -0300

    SE agregaron parametros y funcion de remocion de caracteres raros

--- functions.py
+++ functions.py
@@ -6,6 +6,9 @@ import argparse
 import locale
 from datetime import datetime
 import re 
+import html
+import unicodedata
+
 
 locale.setlocale(locale.LC_ALL, '')
 
@@ -35,7 +38,7 @@ def buscar_fecha(cadena):
       return -1
    
 def readRemoveFile():
-   with open(os.getcwd()+'\\removeStrFromFiles.list', "r") as file:
+   with open(os.getcwd()+'\\remove.list', "r") as file:
       lineas = file.readlines()
    return lineas
 
@@ -68,133 +71,272 @@ def remove_date(cadena):
    return cadena   
 
 
-def remove_first_char(string):
-    if string[0] == "_":
-        return string[1:]
-    return string
+def remove_first_char(cadena):
+   
+   if cadena[0] == "_":
+      cadena= cadena[1:]
+   
+   if cadena[-1] == "_":
+      cadena= cadena[:-1]
+   
+   return cadena
     
 def getLastFolder(cadena):
    directory = os.path.dirname(cadena)
    base = os.path.basename(directory)
    return base
 
-def move_files(src_folder,tar_folder,
-               move_files,copy_files,create_folder,
-               delStrFromFileName,delDateFromFileName,delStrFromFolderName,delDateFromFolderName,
-               addDateToFileName,addFolderToFileName,addFolderToNewFolder):
+def addTextToFile(pathToFile,cadena):
+   file = open(pathToFile,'a')
+   file.write('\n' + cadena)
+   file.close()  
+
+
+def getHeadHtml():
+   cadena="""
+   <!DOCTYPE html>
+<html lang="en">
+<head>
+   <meta charset="UTF-8">
+   <meta http-equiv="X-UA-Compatible" content="IE=edge">
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
+   <title>MDDF2 Procces Files List</title>
+   </head>
+      <body>
+         <table class=\"table table-striped-columns\">
+            <tr>
+               <th>#</th>
+               <th>Source path</th>
+               <th>Target path</th>
+               <th>Status</th>
+               <th>Image</th>
+            </tr>"""
+   return cadena
+
+def escape_html(text):
+   replace_dict = {" ":"&nbsp;","¡":"&iexcl;","¢":"&cent;","£":"&pound;","¤":"&curren;","¥":"&yen;",
+                  "¦":"&brvbar;","§":"&sect;","¨":"&uml;","©":"&copy;","ª":"&ordf;","«":"&laquo;",
+                  "¬":"&not;","­":"&shy;","®":"&reg;","¯":"&macr;","°":"&deg;","±":"&plusmn;",
+                  "²":"&sup2;","³":"&sup3;","´":"&acute;","µ":"&micro;","¶":"&para;","·":"&middot;",
+                  "¸":"&cedil;","¹":"&sup1;","º":"&ordm;","»":"&raquo;","¼":"&frac14;","½":"&frac12;",
+                  "¾":"&frac34;","¿":"&iquest;","À":"&Agrave;","Á":"&Aacute;","Â":"&Acirc;","Ã":"&Atilde;",
+                  "Ä":"&Auml;","Å":"&Aring;","Æ":"&AElig;","Ç":"&Ccedil;","È":"&Egrave;","É":"&Eacute;",
+                  "Ê":"&Ecirc;","Ë":"&Euml;","Ì":"&Igrave;","Í":"&Iacute;","Î":"&Icirc;","Ï":"&Iuml;",
+                  "Ð":"&ETH;","Ñ":"&Ntilde;","Ò":"&Ograve;","Ó":"&Oacute;","Ô":"&Ocirc;","Õ":"&Otilde;",
+                  "Ö":"&Ouml;","×":"&times;","Ø":"&Oslash;","Ù":"&Ugrave;","Ú":"&Uacute;","Û":"&Ucirc;",
+                  "Ü":"&Uuml;","Ý":"&Yacute;","Þ":"&THORN;","ß":"&szlig;","à":"&agrave;","á":"&aacute;",
+                  "â":"&acirc;","ã":"&atilde;","ä":"&auml;","å":"&aring;","æ":"&aelig;","ç":"&ccedil;",
+                  "è":"&egrave;","é":"&eacute;","ê":"&ecirc;","ë":"&euml;","ì":"&igrave;","í":"&iacute;",
+                  "î":"&icirc;","ï":"&iuml;","ð":"&eth;","ñ":"&ntilde;","ò":"&ograve;","ó":"&oacute;",
+                  "ô":"&ocirc;","õ":"&otilde;","ö":"&ouml;","÷":"&divide;","ø":"&oslash;","ù":"&ugrave;",
+                  "ú":"&uacute;","û":"&ucirc;","ü":"&uuml;","ý":"&yacute;","þ":"&thorn;","ÿ":"&yuml;"}
+
+   for char, accented_char in replace_dict.items():
+      text = text.replace(char, accented_char)
+   return text
+
+def strangeCharToNormalChar(text):
+ 
+   replace_dict = {"À":"A;","Á":"A","Â":"A","Ã":"A",
+                     "Ä":"A","Å":"A","Æ":"A","Ç":"C","È":"E","É":"E",
+                     "Ê":"E","Ë":"E","Ì":"I","Í":"I","Î":"I","Ï":"I",
+                     "Ð":"D","Ñ":"N","Ò":"O","Ó":"O","Ô":"O","Õ":"O",
+                     "Ö":"o","×":"x","Ø":" DIAM ","Ù":"U","Ú":"U","Û":"U",
+                     "Ü":"U","Ý":"Y","Þ":"p","ß":"B","à":"a","á":"a",
+                     "â":"a","ã":"a","ä":"a","å":"a","æ":"a","ç":"c",
+                     "è":"e","é":"e","ê":"e","ë":"e","ì":"i","í":"i",
+                     "î":"i","ï":"i","ð":"o","ñ":"n","ò":"o","ó":"o",
+                     "ô":"o","õ":"o","ö":"o","÷":" div ","ø":" diam ","ù":"u",
+                     "ú":"u","û":"u","ü":"o","ý":"y","þ":"p","ÿ":"y"}
+
+   for char, accented_char in replace_dict.items():
+      text = text.replace(char, accented_char)
+   return text  
+ 
+
    
-        for dirpath, dirnames, filenames in os.walk(src_folder):
-            for file in filenames:
-               file_path = os.path.join(dirpath, file)
-               newFile=file
-               print ("===========================================================================")  
+
+
+def getLineHtml(number,srcPath,tarPath,status,image):
+   print(srcPath)
+   cadena=""
+   cadena=cadena+"<tr>"
+   cadena=cadena+"<td>"+number+"</td>"
+   cadena=cadena+"<td>"+escape_html(srcPath)+"</td>"
+   cadena=cadena+"<td>"+escape_html(tarPath)+"</td>"
+   cadena=cadena+"<td>"+status+"</td>"      
+   cadena=cadena+"<td><img src=\""+image+"\" width=\"80\" ></td>"      
+   cadena=cadena+"</tr>"
+   return cadena
+   
+def getFooterHtml():
+   cadena="""</table>
+   </body>
+   </html>
+   """
+   return cadena
+
+
+                       
+def move_files(src_folder,tar_folder,
+               move_files,copy_files,create_folder,demo,
+               delStrFromFileName,delDateFromFileName,delStrFromFolderName,delDateFromFolderName,
+               addDateToFileName,addFolderToFileName,addFolderToNewFolder,
+               removeChar):
+   cont=0
+
+   if (create_folder or move_files or copy_files or demo):                 
+      addTextToFile(src_folder+'\\process_demo.html', getHeadHtml())
                
-               lastFolder=getLastFolder(file_path)
+   if (create_folder or move_files or copy_files):            
+      addTextToFile(tar_folder+'\\process_demo.html', getHeadHtml())
+                       
+   
+                                   
+   for dirpath, dirnames, filenames in os.walk(src_folder):
+      for file in filenames:
+         file_path = os.path.join(dirpath, file)
+         newFile=file
+         cont=cont+1
+         print ("===========================================================================")  
+         print ("[ "+str(cont)+" ]")
                
-               print("file_path="+file_path)
-               print("lastFolder="+lastFolder)
+         lastFolder=strangeCharToNormalChar(getLastFolder(file_path))
+               
+         print("file_path="+file_path)
+         print("lastFolder="+lastFolder)
                 # --------------------------------------------------               
                # -----DELETE----
                
                 # Delete string contained in the file "removeStrFromFiles" 
                # from the file name
-               if (delStrFromFileName==True):
-                  newFile=cleanStrangeChars(readRemoveFile(),newFile) 
+         if (delStrFromFileName==True):
+            newFile=cleanStrangeChars(readRemoveFile(),newFile) 
                
                # Delete the date from the file name                         
-               if (delDateFromFileName==True):
-                  newFile=remove_date(newFile) 
+         if (delDateFromFileName==True):
+            newFile=remove_date(newFile) 
 
-               # Delete string contained in the file "removeStrFromFiles" 
-               # from the folder name                     
-               if (delStrFromFolderName==True):
-                  lastFolder=cleanStrangeChars(readRemoveFile(),lastFolder) 
+         # Delete string contained in the file "removeStrFromFiles" 
+         # from the folder name                     
+         if (delStrFromFolderName==True):
+            lastFolder=cleanStrangeChars(readRemoveFile(),lastFolder) 
                   
                # Delete the date from the folder name                                              
-               if (delDateFromFolderName==True):
-                  lastFolder=remove_date(lastFolder) 
+         if (delDateFromFolderName==True):
+            lastFolder=remove_date(lastFolder) 
                   
-               # --------------------------------------------------                  
-               # ------------------ADD-----------------------------
-               # Add de date to the start of file name
-               addToNewFile=""
-               addToNewFolder=""
+         # --------------------------------------------------                  
+         # ------------------ADD-----------------------------
+         # Add de date to the start of file name
+         addToNewFile=""
+         addToNewFolder=""
                
-               if (addDateToFileName==True):   
-                  print ("Adding date = [ "+modify_date.strftime("%Y-%m-%d")+" ] to file name")
-                  addToNewFile=addToNewFile+"_"+modify_date.strftime("%Y-%m-%d")
+         if (addDateToFileName==True):   
+            print ("Adding date = [ "+modify_date.strftime("%Y-%m-%d")+" ] to file name")
+            addToNewFile=addToNewFile+"_"+modify_date.strftime("%Y-%m-%d")
                   
 
-               # Add de original folder name to the begin of the new file name
-               if (addFolderToFileName==True):  
-                  print ("Adding last folder = [ "+lastFolder+" ] to file name")
-                  addToNewFile=addToNewFile+"_"+lastFolder
+         # Add de original folder name to the begin of the new file name
+         if (addFolderToFileName==True):  
+            print ("Adding last folder = [ "+lastFolder+" ] to file name")
+            addToNewFile=addToNewFile+"_"+lastFolder
                                              
-               # Add de original folder name to the begin of the new folder name
-               if (addFolderToNewFolder==True):   
-                  print ("Adding last folder = [ "+lastFolder+"  ] to folder name")
-                  addToNewFolder=addToNewFolder+"_"+lastFolder
+         # Add de original folder name to the begin of the new folder name
+         if (addFolderToNewFolder==True):   
+            print ("Adding last folder = [ "+lastFolder+"  ] to folder name")
+            addToNewFolder=addToNewFolder+"_"+lastFolder
+                              
+         modify_date = datetime.fromtimestamp(os.path.getmtime(file_path))                
+         dest_folder = os.path.join(tar_folder, modify_date.strftime("%Y"))
+         
+         if(removeChar==True):
+            dest_folder=strangeCharToNormalChar(dest_folder)                           
+         
+         if (move_files or copy_files or create_folder):
+            createFolder(dest_folder)
+               
+         dest_folder = dest_folder + "\\" + modify_date.strftime("%m - %B")
+         
+         if(removeChar==True):
+            dest_folder=strangeCharToNormalChar(dest_folder)                                      
+         
+         if (move_files or copy_files or create_folder):
+            createFolder(dest_folder)                  
+                 
+         dest_folder = dest_folder + "\\" + modify_date.strftime("%Y-%m-%d [ %A ]")
+            
+            
+             #concateno y creo ruta destino y nombre de archivo destino completo 
+         newFile=addToNewFile+newFile
+         
+         dest_folder=dest_folder+addToNewFolder
+         dest_folder=dest_folder.strip()
+         dest_folder=remove_first_char(dest_folder)
+            
+         newFile=remove_first_char(newFile)
+         
+         if (move_files):
+            print("Moving Files...")
+               
+         if (copy_files):
+            print("Copy Files...")                  
+               
+         if (create_folder):
+            print("Creating Folder...")
+         if(removeChar==True):
+            dest_folder=strangeCharToNormalChar(dest_folder)                           
+         
+         if (move_files or copy_files or create_folder):
+            createFolder(dest_folder)                     
+            
+
+             
+         print ("from: "+file_path)        
+         print ("to:" +dest_folder+"\\" +file)    
+            
+         rutaArchivo=os.path.join(dest_folder, newFile)
+         
+         if(removeChar==True):            
+            rutaArchivo=strangeCharToNormalChar(rutaArchivo) 
+            # Mover el archivo a la carpeta de destino
+         band="NULL" 
+         if (demo):
+            print("Creating Demo...") 
+            addTextToFile(src_folder+'\\process_demo.html',getLineHtml(str(cont),
+            file_path,rutaArchivo,"OK",file_path))
+            band="SIMULATION"      
+            
+         
+            
+         if (move_files or copy_files):
+          
+            try:            
+               if  (move_files):
                   
-              
-               
-               modify_date = datetime.fromtimestamp(os.path.getmtime(file_path))
-                
-               dest_folder = os.path.join(tar_folder, modify_date.strftime("%Y"))
-               
-               
-               if (move_files or copy_files or create_folder):
-                  createFolder(dest_folder)
-                  
-                    
-               dest_folder = dest_folder + "\\" + modify_date.strftime("%m - %B")
-               
-               if (move_files or copy_files or create_folder):
-                  createFolder(dest_folder)                  
-                    
-               dest_folder = dest_folder + "\\" + modify_date.strftime("%Y-%m-%d [ %A ]")
-               
-               
-                #concateno y creo ruta destino y nombre de archivo destino completo 
-               newFile=addToNewFile+newFile
-               dest_folder=dest_folder+addToNewFolder
-               dest_folder=dest_folder.strip()
-               dest_folder=remove_first_char(dest_folder)
-               
-               newFile=remove_first_char(newFile)
-               if (move_files or copy_files or create_folder):
-                  createFolder(dest_folder)                     
-               
-               
-    
-               if (move_files):
-                  print("Moving Files...")
-                  
+                  shutil.move(file_path, rutaArchivo)
+                  band="Move OK"
                if (copy_files):
-                  print("Copy Files...")                  
-                  
-               if (create_folder):
-                  print("Creating Folder...")
-                                       
-               print ("from: "+file_path)        
-               print ("to:" +dest_folder+"\\" +file)    
-               # Mover el archivo a la carpeta de destino
-               if (move_files or copy_files):
-                  try:            
-                     if  (move_files):
-                        shutil.move(file_path, os.path.join(dest_folder, newFile))
-                     if (copy_files):
-                        shutil.copy(file_path, os.path.join(dest_folder, newFile))
-                  except:
-                     f1 = open(src_folder+'\mddf2_move_log.txt','a')
-                     f1.write('\n' + "I can't move or copy from: "+file_path+"\\" +file)
-                     f1.write('\n' + "I can't move or copy to  : "+dest_folder+"\\" +newFile)
-                     f1.close()  
-                     
-                     f2 = open(tar_folder+'\mddf2_move_log.txt','a')
-                     f2.write('\n' + "I can't move or copy from: "+file_path+"\\" +file)
-                     f2.write('\n' + "I can't move or copy to  : "+os.path.join(dest_folder, newFile))
-                     
-                     f2.close()                    
-                     print ("I can't move or copy: "+file_path+"\\" +file)
-                  
-                  
+                  shutil.copy(file_path, rutaArchivo)
+                  band="Copy OK"
+            except:
+               addTextToFile(src_folder+'\\mddf2_move_log.txt',"I can't move or copy from: "+file_path+"\\" +file)
+               addTextToFile(src_folder+'\\mddf2_move_log.txt',"I can't move or copy to  : "+rutaArchivo)                                          
+               addTextToFile(tar_folder+'\\mddf2_move_log.txt',"I can't move or copy from: "+file_path+"\\" +file)
+               addTextToFile(tar_folder+'\\mddf2_move_log.txt',"I can't move or copy to  : "+rutaArchivo) 
+               band="FAIL"                     
+               print ("I can't move or copy: "+file_path+"\\" +file)
+         
+         if (demo or move_files or copy_files):
+            
+            addTextToFile(src_folder+'\\process_demo.html',getLineHtml(str(cont),file_path,rutaArchivo,band,file_path))   
+            
+         if (create_folder or move_files or copy_files):
+            addTextToFile(tar_folder+'\\process_demo.html',getLineHtml(str(cont),file_path,rutaArchivo,band,file_path))   
+
+   if (demo or move_files or copy_files):                 
+      addTextToFile(src_folder+'\\process_demo.html', getFooterHtml())
+               
+   if (create_folder or move_files or copy_files):            
+      addTextToFile(tar_folder+'\\process_demo.html', getFooterHtml())                  
--- mddf2.py
+++ mddf2.py
@@ -17,18 +17,21 @@ locale.setlocale(locale.LC_ALL, '')
 parser = argparse.ArgumentParser()
 parser.add_argument("sd", help="Source directory")
 parser.add_argument("td", help="Target directory")
-parser.add_argument('-m',   '--move_file', action='store_true', help='move files')
-parser.add_argument('-c',   '--copy_file', action='store_true', help='copy files')
+parser.add_argument('-m',   '--move_file',     action='store_true', help='move files')
+parser.add_argument('-c',   '--copy_file',     action='store_true', help='copy files')
 parser.add_argument('-f',   '--create_folder', action='store_true', help='create folders')
-parser.add_argument('-dsf', '--delStringFilename', action='store_true', help='Delete String in file remove(default: "remove.list") from target filename')
-parser.add_argument('-ddf', '--delDateFilename', action='store_true',   help='Delete date string from target filename')
-parser.add_argument('-dsd', '--delStringDirectory', action='store_true', help='Delete string in file remove(default: "remove.list") from target directory')
+parser.add_argument('-d',   '--demo',          action='store_true', help='Create a html output')
+parser.add_argument('-dsf', '--delStringFilename', action='store_true', help='Delete String in file name, remove the strings in ("remove.list" file)')
+parser.add_argument('-ddf', '--delDateFilename', action='store_true',   help='Delete date string from target flename')
+parser.add_argument('-dsd', '--delStringDirectory', action='store_true', help='Delete string in directory name, remove the string in(default: "remove.list")')
 parser.add_argument('-ddd', '--delDateDirectory', action='store_true',  help='Delete date string  from target directory(if source directory was added)')
 
 
-parser.add_argument('-adf', '--addDateToFileName', action='store_true', help='Add date to filename')
-parser.add_argument('-aff', '--addFolderToFileName', action='store_true',   help='Add folder to filename')
-parser.add_argument('-afd', '--addFolderToNewFolder', action='store_true',   help='Add source folder to new folder name')
+
+parser.add_argument('-adf', '--addDateToFileName',      action='store_true',    help='Add date to filename')
+parser.add_argument('-aff', '--addFolderToFileName',    action='store_true',   help='Add folder to filename')
+parser.add_argument('-afd', '--addFolderToNewFolder',   action='store_true',   help='Add source folder to new folder name')
+parser.add_argument('-rmc', '--removeChar',             action='store_true',   help='Remove a normalize strange char')
 
 #parser.add_argument("dn", help="add the date to begint of the file name")
 #parser.add_argument("r", help="add the date to begint of the file name")
@@ -44,7 +47,8 @@ if args.sd:
         tar_folder=src_folder
   
     move_files(src_folder,tar_folder,
-               args.move_file,args.copy_file,args.create_folder,
+               args.move_file,args.copy_file,args.create_folder,args.demo,
                args.delStringFilename,args.delStringFilename,args.delStringDirectory,args.delDateDirectory,
-               args.addDateToFileName,args.addFolderToFileName,args.addFolderToNewFolder)
+               args.addDateToFileName,args.addFolderToFileName,args.addFolderToNewFolder,
+               args.removeChar)
     
